345a1deb80f4e9139fa2c2169b3a9e3add61f41e
Marco Asbreuk <marco@itsasbreuk.nl>
2015-01-12 01:13:58 +0100

    intermediate saving

---
+++ i-tabpane.js
@@ -0,0 +1,30 @@
+/*
+* attributes:
+* value, expanded, primary-button
+*/
+require('polyfill/polyfill-base.js');
+require('js-ext/lib/string.js');
+require('css');
+require('./css/i-tabpane.css');
+
+var TRANS_TIME_SHOW = 0.3,
+    TRANS_TIME_HIDE = 0.1,
+    CLASS_ITAG_RENDERED = 'itag-rendered';
+
+module.exports = function (window) {
+
+    "use strict";
+    var itagsCore = require('itags.core')(window),
+        Event = require('event-dom')(window);
+
+    // require('focusmanager')(window);
+    require('i-item')(window);
+    require('i-head')(window);
+
+    window.document.createTag('i-tabpane');
+
+    // window.HTMLElement.prototype.subClass('i-tabpane', function() {
+        // console.warn('constructor itabpane');
+    // });
+
+};
